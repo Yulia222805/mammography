2d8fc711b14ef1f3a341fd0005d50e8a4f9e5d80
Yulia222805 <138562515+Yulia222805@users.noreply.github.com>
2025-06-02 13:15:42 +0300

    add files

---
+++ model_set.py
@@ -0,0 +1,98 @@
+# model_utils.py
+
+import cv2
+import numpy as np
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+def remove_artifacts(image, mask):
+    _, thresh = cv2.threshold(image, 20, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if not contours:
+        return image, mask
+    cnt = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(cnt)
+    return image[y:y+h, x:x+w], mask[y:y+h, x:x+w]
+
+def pad_to_square(img):
+    h, w = img.shape
+    diff = abs(h - w)
+    if h > w:
+        pad_l = diff // 2
+        pad_r = diff - pad_l
+        return cv2.copyMakeBorder(img, 0, 0, pad_l, pad_r, cv2.BORDER_CONSTANT)
+    else:
+        pad_t = diff // 2
+        pad_b = diff - pad_t
+        return cv2.copyMakeBorder(img, pad_t, pad_b, 0, 0, cv2.BORDER_CONSTANT)
+
+# def predict_mask(model, image_path, device='cuda', threshold=0.5):
+#     model.eval()
+
+#     image = cv2.imread(image_path, 0)  # grayscale
+#     if image is None:
+#         raise FileNotFoundError(f"Не удалось загрузить изображение: {image_path}")
+
+#     # image_cropped, _ = remove_artifacts(image, np.zeros_like(image))
+#     # image_padded = pad_to_square(image_cropped)
+#     image_resized = cv2.resize(image, (512, 512))
+
+#     cl_img = image_resized.astype(np.float32) / 255.0
+
+#     transform = A.Compose([
+#         A.Normalize(mean=[0.1454], std=[0.2365], max_pixel_value=1.0, p=1),
+#         ToTensorV2()
+#     ])
+#     augmented = transform(image=cl_img)
+#     input_tensor = augmented['image'].unsqueeze(0).to(device)
+
+#     with torch.no_grad():
+#         output = model(input_tensor)
+#     pred_mask = torch.sigmoid(output).cpu().numpy()[0, 0] > threshold
+#     pred_mask = (pred_mask * 255).astype(np.uint8)
+
+#     return image_resized, pred_mask
+
+def predict_mask(model, image_np, device='cpu', threshold=0.5):
+    """
+    :param model: обученная модель
+    :param image_np: numpy array (grayscale), исходное изображение
+    :param device: 'cpu' или 'cuda'
+    :param threshold: порог бинаризации маски
+    :return: resized_image, pred_mask
+    """
+
+    # image_np — это уже numpy array, НЕ нужно читать через imread
+    image = image_np.copy()
+
+    # Обработка
+    image_cropped, _ = remove_artifacts(image, np.zeros_like(image))
+    image_padded = pad_to_square(image_cropped)
+    image_resized = cv2.resize(image_padded, (512, 512))
+
+    # image_resized = cv2.resize(image, (512, 512))
+
+    cl_img = image_resized.astype(np.float32) / 255.0
+
+    transform = A.Compose([
+        A.Normalize(mean=[0.1454], std=[0.2365], max_pixel_value=1.0, p=1),
+        ToTensorV2()
+    ])
+    augmented = transform(image=cl_img)
+    input_tensor = augmented['image'].unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        output = model(input_tensor)
+    pred_mask = torch.sigmoid(output).cpu().numpy()[0, 0] > threshold
+    pred_mask = (pred_mask * 255).astype(np.uint8)
+
+    return image_resized, pred_mask
+
+def draw_contour_on_image(image_resized, pred_mask):
+    # Преобразуем изображение для отрисовки
+    image_color = cv2.cvtColor(image_resized, cv2.COLOR_GRAY2BGR)
+    contours, _ = cv2.findContours(pred_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(image_color, contours, -1, (255, 0, 0), thickness=2)
+
+    return image_color
---
+++ tabs.py
@@ -0,0 +1,247 @@
+import streamlit as st
+from datetime import datetime
+from PIL import Image
+import os
+import torch
+import numpy as np
+import cv2
+import segmentation_models_pytorch as smp
+
+from model_set import predict_mask, draw_contour_on_image
+
+st.set_page_config(page_title="Маммограмма", layout="wide")
+# st.header("Анализ маммограммы") #title
+
+if 'upload_image' not in st.session_state:
+     st.session_state.upload_image = False
+
+if 'show_predict' not in st.session_state:
+     st.session_state.show_predict = False
+
+if 'original_image_name' not in st.session_state:
+    st.session_state.original_image_name = ''
+
+if 'image_np' not in st.session_state:
+    st.session_state.image_np = None
+
+if 'annotated_image' not in st.session_state:
+    st.session_state.annotated_image = None
+
+# if 'active_tab' not in st.session_state:
+    # st.session_state.active_tab = "Загруженная маммограмма"
+global new_size
+
+@st.cache_resource
+def load_model():
+    model = smp.UnetPlusPlus(
+        encoder_name="timm-efficientnet-b2",
+        encoder_weights=None,
+        in_channels=1,
+        classes=1,
+    )
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.load_state_dict(torch.load('unet_best_7828.pth', map_location=device))
+    model = model.to(device)
+    # model.eval()
+    return model
+
+model = load_model()
+
+
+with st.sidebar:
+    st.header("Загрузка изображения")
+    uploaded_file = st.file_uploader(
+        "Выберите изображение", 
+        type=["png", "jpg", "jpeg"])
+
+col1, col2 = st.columns([2, 1]) 
+
+with col1:
+        # tab1, tab2 = st.tabs(["Загруженная маммограмма", "Предсказанная"])
+        tab_options = ["Загруженная маммограмма", "Прогнозирование"]
+        # default_index = tab_options.index(st.session_state.active_tab) if st.session_state.active_tab in tab_options else 0
+
+        tab1, tab2 = st.tabs(tab_options)
+        
+        with tab1:
+                if uploaded_file is not None:
+                    image = Image.open(uploaded_file).convert('L')
+                    original_width, original_height = image.size
+
+                    new_size = (int(original_width / 0.83), int(original_height / 0.83))
+                    resized_image = image.resize(new_size)
+
+                    image_np = np.array(image)
+                    st.session_state.image_np = image_np
+                    
+                    if st.session_state.original_image_name != uploaded_file.name:
+                        st.session_state.original_image_name = uploaded_file.name
+                        st.session_state.annotated_image = None
+                        st.session_state.show_predict = False
+
+                    st.image(resized_image,
+                            # use_container_width=True
+                            )
+                    st.session_state.upload_image = True
+                    
+                    # if st.sidebar.button("Сделать прогноз"):
+                    #     st.session_state.show_predict = True
+                    #     # st.session_state.active_tab = "Предсказанная"
+                    #     st.rerun()
+                    with st.sidebar:
+                        col1_, col2_, col3_ = st.columns([1, 3, 1])
+                        with col2_:
+                            predict = st.button("Сделать прогноз", 
+                                                # use_container_width=True
+                                                )
+                        if predict:
+                            st.session_state.show_predict = True
+                                # st.session_state.active_tab = "Предсказанная"
+                            st.rerun()   
+                else:
+                    st.info("Загрузите изображение")
+
+        with tab2:
+            if uploaded_file is None:
+                if 'original_image_name' in st.session_state and st.session_state.original_image_name != '':
+                    st.session_state.annotated_image = None
+                    st.session_state.image_np = None
+                    st.session_state.upload_image = False
+                    st.session_state.show_predict = False
+                    st.session_state.original_image_name = ''
+                    # st.warning("Нет изображения для анализа")
+                    # st.info("Изображение удалено")
+                # else:
+                st.warning("Нет изображения для анализа")
+
+            elif st.session_state.show_predict:
+                if st.session_state.image_np is not None:
+                    if st.session_state.annotated_image is None:
+                        with st.spinner("Выполняется анализ..."):
+                            try:
+                                image_resized, pred_mask = predict_mask(model, st.session_state.image_np, device='cpu')
+                                annotated_image = draw_contour_on_image(image_resized, pred_mask)
+
+                                # st.session_state.annotated_image = annotated_image
+
+                                annotated_pil = Image.fromarray(annotated_image)
+                                annotated_resized = annotated_pil.resize(new_size)  # <-- Применяем resize
+                                st.session_state.annotated_image = np.array(annotated_resized)
+                            except Exception as e:
+                                 st.error(f"Ошибка при выполнении проноза: {e}")
+                    if st.session_state.annotated_image is not None:
+                        st.image(st.session_state.annotated_image, 
+                            #  use_container_width=True, 
+                            #  channels="BGR"
+                             )
+                    else:
+                        st.warning("Не удалось загрузить прогноз")
+            else:
+                st.info("Выполните прогноз для анализа")
+
+
+from datetime import datetime
+import os
+
+# Папка для сохранения
+# SAVE_DIR = "saved_data"
+# os.makedirs(SAVE_DIR, exist_ok=True)
+
+# with col2:
+#     st.markdown("### Информация о пациенте")
+
+#     with st.form(key="patient_form"):
+#         full_name = st.text_input("ФИО пациента")
+#         birth_date = st.date_input("Дата рождения", value=None, format="DD/MM/YYYY")
+#         visit_date = st.date_input("Дата приёма", value=None, format="DD/MM/YYYY")
+#         description = st.text_area("Описание/заметки врача", height=200)
+
+#         submit_button = st.form_submit_button(label="Сохранить данные")
+
+#     if submit_button:
+#         data = []
+#         if full_name:
+#             data.append(f"ФИО пациента: {full_name}")
+#         if birth_date:
+#             data.append(f"Дата рождения: {birth_date.strftime('%d-%m-%Y')}")
+#         if visit_date:
+#             data.append(f"Дата приёма: {visit_date.strftime('%d-%m-%Y')}")
+#         if description:
+#             data.append(f"Описание: {description}")
+
+#         if data:
+#             # timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+#             # file_name = f"patient_{timestamp}.txt"
+#             # file_path = os.path.join(SAVE_DIR, file_name)
+
+#             # # Сохраняем файл ЛОКАЛЬНО на сервере
+#             # with open(file_path, "w", encoding="utf-8") as f:
+#             #     f.write("\n".join(data))
+
+#             # st.success(f"Данные успешно сохранены: `{file_name}`")
+
+#             # st.session_state.file_content = "\n".join(data)
+#             # st.session_state.file_name = file_name
+#             file_content = "\n".join(data)
+#             file_name = f"patient_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+
+#             st.download_button(
+#                 label="Скачать файл",
+#                 data=file_content,
+#                 file_name=file_name,
+#                 mime="text/plain"
+#             )
+#         else:
+#             st.warning("Нет данных для сохранения.")
+
+#     # Эта часть ВНЕ формы — можно использовать download_button
+#     # if 'file_content' in st.session_state and 'file_name' in st.session_state:
+#     #     st.download_button(
+#     #         label="Скачать файл",
+#     #         data=st.session_state.file_content,
+#     #         file_name=st.session_state.file_name,
+#     #         mime="text/plain"
+#     #     )
+
+with col2:
+    st.markdown("### Информация о пациенте")
+
+    with st.form(key="patient_form"):
+        full_name = st.text_input("ФИО пациента")
+        birth_date = st.date_input("Дата рождения", value=None, format="DD/MM/YYYY")
+        visit_date = st.date_input("Дата приёма", value=None, format="DD/MM/YYYY")
+        description = st.text_area("Описание/заметки врача", height=200)
+
+        submit_button = st.form_submit_button(label="Сохранить данные")
+
+    if submit_button:
+        data = []
+        if full_name:
+            data.append(f"ФИО пациента: {full_name}")
+        if birth_date:
+            data.append(f"Дата рождения: {birth_date.strftime('%d-%m-%Y')}")
+        if visit_date:
+            data.append(f"Дата приёма: {visit_date.strftime('%d-%m-%Y')}")
+        if description:
+            data.append(f"Описание: {description}")
+
+        if data:
+            file_content = "\n".join(data)
+            file_name = f"patient_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt"
+
+            # Сохраняем в session_state
+            st.session_state.file_content = file_content
+            st.session_state.file_name = file_name
+
+            st.success(f"Данные сохранены. Вы можете скачать их ниже.")
+        else:
+            st.warning("Нет данных для сохранения.")
+
+    # Всегда проверяем, есть ли что скачивать
+    if 'file_content' in st.session_state and 'file_name' in st.session_state:
+        st.download_button(
+            label="Скачать файл",
+            data=st.session_state.file_content,
+            file_name=st.session_state.file_name,
+            mime="text/plain"
+        )
